3cf125fea5d4fc46e88f19de59d849fbb923270b
Melbin <melbinbabu66@gmail.com>
2024-10-04 03:46:57 +0530

    Register DiaryManagerBloc in HomeScreenView

--- lib/features/new_diary/presentation/widgets/new_diary_page_widgets/home_screen_view.dart
+++ lib/features/new_diary/presentation/widgets/new_diary_page_widgets/home_screen_view.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diary_app/features/new_diary/data/datasources/diary_remote_data_source.dart';
+import 'package:diary_app/features/new_diary/data/repositories/diary_repository_impl.dart';
+import 'package:diary_app/features/new_diary/domain/usecases/add_diary_entry.dart';
 import 'package:diary_app/features/new_diary/presentation/bloc/diary_manager/diary_manager_bloc.dart';
 import 'package:diary_app/features/new_diary/presentation/widgets/input_field.dart';
 import 'package:diary_app/features/new_diary/presentation/widgets/new_diary_page_widgets/entry_section.dart';
@@ -9,27 +13,31 @@ class HomeScreenView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<DiaryManagerBloc, DiaryManagerState>(
-      listener: (context, state) {
-        if (state is DiaryAdded) {
-          ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Diary entry added!')));
-        } else if (state is DiaryAddingFailed) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(const SnackBar(content: Text('failed to add')));
-        }
-      },
-      child: Column(
-        children: [
-          const InputField(
-            hintText: 'Date',
-            leadingIcon: Icon(Icons.calendar_month),
-          ),
-          const InputField(
-            hintText: 'Title',
-          ),
-          EntrySection()
-        ],
+    return BlocProvider(
+      create: (context) => DiaryManagerBloc(AddDiaryEntry(DiaryRepositoryImpl(
+          DiaryRemoteDataSourceImpl(FirebaseFirestore.instance)))),
+      child: BlocListener<DiaryManagerBloc, DiaryManagerState>(
+        listener: (context, state) {
+          if (state is DiaryAdded) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Diary entry added!')));
+          } else if (state is DiaryAddingFailed) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text('Failed to add')));
+          }
+        },
+        child: Column(
+          children: [
+            const InputField(
+              hintText: 'Date',
+              leadingIcon: Icon(Icons.calendar_month),
+            ),
+            const InputField(
+              hintText: 'Title',
+            ),
+            EntrySection(),
+          ],
+        ),
       ),
     );
   }
